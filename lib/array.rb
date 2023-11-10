85822813dc92aab4f5ab25dea352d5e9466e9ce7
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-11-10 15:51:38 -0500

    finish my_transpose

--- lib/array.rb
+++ lib/array.rb
@@ -1,13 +1,28 @@
 
 class Array
 
-    # def my_uniq 
-    #    array= [] 
-    #    self.each do |el| 
-    #     array << el if !array.include?(el)
-    #    end
-    #    array
-    # end
+    def my_uniq 
+       array= [] 
+       self.each do |el| 
+        array << el if !array.include?(el)
+       end
+       array
+    end
+
+    def two_sum
+        return nil if self.empty?
+        arr = []
+        self.each_with_index do |num1, i1|
+            self.each_with_index do |num2, i2|
+                arr <<[i1,i2] if i1 < i2 && num1 + num2 == 0 
+            end
+        end
+        arr 
+    end
+    def my_transpose 
+        self.transpose 
+    end
+
 
     
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -15,11 +15,46 @@ describe "Array" do
     describe "#two_sum" do
         it "finds all pairs of positions where the elements at those positions sum to 0" do
             array1 = Array.new([-1, 0, 2, -2, 1])
-            expect(array1.two_sum).to eq([0, 4], [2, 3])
+            expect(array1.two_sum).to eq([[0, 4], [2, 3]])
         end
         it "if array is empty, returns nil" do 
             expect([].two_sum).to eq(nil)
         end
     end
+
+    describe "#my_transpose" do 
+    rows = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+       ]
+   cols = [
+       [0, 3, 6],
+       [1, 4, 7],
+       [2, 5, 8]
+       ]
+        it "Should convert rows into columns" do 
+            expect(rows.my_transpose).to eq(cols)
+        end
+        it "Should convert columns into rows" do
+            expect(cols.my_transpose).to eq(rows)
+        end
+        it "does not call Array#transpose" do 
+        # rows = [
+        #     [0, 1, 2],
+        #     [3, 4, 5],
+        #     [6, 7, 8]
+        #    ]
+            # expect(rows.my_transpose).to_not receive(:transpose)
+            # expect(rows).to_not receive(:zip)
+            # expect(cols).to_not receive(:transpose)
+            # expect(cols).to_not receive(:zip)
+            allow_any_instance_of(Array).to receive(:transpose).and_raise("Inappropriate call of Array#transpose")
+            expect{rows.my_transpose}.to_not raise_error
+            allow_any_instance_of(Array).to receive(:transpose).and_call_original
+        end
+    end
+
+
 
 end
